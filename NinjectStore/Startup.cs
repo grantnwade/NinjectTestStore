ac0a9c5fc59c99a9f9053a121196da239b8769f7
Grant <grantnwade@gmail.com>
2016-02-15 13:21:43 -0600

    Initial Commit

---
+++ NinjectStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NinjectStore.Startup))]
+namespace NinjectStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
